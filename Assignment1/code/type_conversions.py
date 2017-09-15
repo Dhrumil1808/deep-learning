57de41e3a5952f0dead4b094563015e3d1ff8f0f
Dhrumil1808 <dhrml.shah@gmail.com>
2017-09-15 15:12:37 -0700

    type conversions

---
+++ Assignment1/code/type_conversions.py
@@ -0,0 +1,9 @@
+string = "100"
+integer= int("100")  
+bases=int(string,2) # converted the string to base 2
+decimal= 3
+decimal= float(integer)  #type casted from integer to float
+
+print(integer)
+print(decimal)
+print(bases)
