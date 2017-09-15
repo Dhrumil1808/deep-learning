9f185cc807e36bd7f887a662677ff4e2e7cda9df
Dhrumil1808 <dhrml.shah@gmail.com>
2017-09-15 15:10:40 -0700

    conditions

---
+++ Assignment1/code/conditions.py
@@ -0,0 +1,7 @@
+a=10;
+if(a <10): #evaluated when input is less than 10
+    print "a is less than 10"
+elif( a ==10): #evaluated when input equals 10
+    print "a is equal to 10"
+else: #evaulated when input is greater than 10
+    print "a is greater than 10"
