5bf0c0473b7fa9e9feb033d76eb6102f66968539
Dhrumil1808 <dhrml.shah@gmail.com>
2017-09-15 15:17:21 -0700

    objects

---
+++ Assignment1/code/objects.py
@@ -0,0 +1,6 @@
+class basic:
+    def function(self):
+        print ("this is called from a function")
+        
+object1=basic(); #object of class "basic" is created
+object1.function()   #function called with the help of object
